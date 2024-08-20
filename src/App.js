79bf419ba649f53628ddc460486ecfcec9136708
Daniel <espartan.95.os@gmail.com>
2024-08-19 22:17:41 -0500

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Home from './pages/Home';
+import FlightSearch from './components/FlightSearch';
 
-function App() {
+const App = () => {
+  const [searchParams, setSearchParams] = useState(null);
+
+  const handleSearch = () => {
+      setSearchParams(); 
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <FlightSearch />
+       <Home />
     </div>
+
+   
+      /*<div>
+          <h1>Busca tu vuelo</h1>
+          <Home onSearch={handleSearch} /> {}
+          {searchParams && <FlightSearch searchParams={searchParams} />}
+      </div>*/
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const Home = ({ onSearch }) => { 
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const onSubmit = handleSubmit((data) => {
+        onSearch(data); 
+        reset();
+    });
+
+
+    return (
+        <form onSubmit={onSubmit}>
+
+            <label htmlFor="origen">De donde viajas</label>
+            <input 
+              type="text" 
+              id="origen"
+              {...register('origen', {required: true})}
+        />
+        {errors.origen && <span>Campo requerido</span>}
+<br />
+        <label
+            htmlFor="destino">A donde vas
+        </label>
+        <input type="text" 
+            {...register("destino", {required: true})}
+        />
+        {errors.destino && <span>Campo requerido</span>}
+
+<br />
+
+        <label
+            htmlFor="horario">hora de salida
+        </label>
+        <input type="time" 
+            {...register("horario", {required: true})}
+        />
+        {errors.horario && <span>Campo requerido</span>}
+
+<br />
+
+        <label
+            htmlFor="fecha">Cuando viajas
+        </label>
+        <input type="date" 
+            {...register("fecha", {required: true})}
+        />
+        {errors.fecha && <span>Campo requerido</span>}
+
+<br />
+
+        <label
+            htmlFor="terminos">Acepto terminos y condiciones
+        </label>
+        <input type="checkbox" 
+            {...register("terminos", {required: true})}
+        />
+        {errors.terminos && <span>Campo requerido</span>}
+
+<br />
+<br />
+        <button type="submit">Enviar</button>
+
+        </form>
+
+        )
+
+
+}
+
+
+
+export default Home;
